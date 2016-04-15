7244a1134f06ff955c7f4827e8a9610b718c2e0a
SEAN CHAMPAGNE <CHAMPAGNE.SEAN@GMAIL.COM>
2016-04-15 14:25:24 -0700

    functions problem 3 displaying sums and product totals of 3 variables

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-function multiply(sum1, sum2) {
-  var total = document.getElementById('multiplyTotal');
-  total = (sum1 * sum2);
-  console.log('The total of ' + sum1 + ' and ' + sum2 + ' is ' + total + '.');
-  multiplyTotal.textContent = 'The total of ' + sum1 + ' and ' + sum2 + ' is ' + total + '.';
+function sumAndMultiply(var1, var2, var3) {
+  var total = document.getElementById('sumAndMultiplyTotal');
+  totalSum = (var1 + var2 + var3);
+  totalMultiply = (var1 * var2 * var3);
+  console.log('The total of ' + var1 + ' and ' + var2 + ' and ' + var3 + ' is ' + totalSum + '.');
+  sumAndMultiplyTotal.textContent = 'The total of ' + var1 + ' and ' + var2 + ' and ' + var3 + ' is ' + totalSum + '.';
+  console.log('The numbers ' + var1 + ' and ' + var2 + ' and ' + var3 + ' product is ' + totalMultiply + '.');
+  sumAndMultiplyTotal.textContent = 'The numbers ' + var1 + ' and ' + var2 + ' and ' + var3 + ' product is ' + totalMultiply + '.';
 }
