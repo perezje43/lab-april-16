aa0f073d7c8de3ca8fb688b3013fccf4066f2e8b
SEAN CHAMPAGNE <CHAMPAGNE.SEAN@GMAIL.COM>
2016-04-15 15:26:03 -0700

    updating my life issues

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+function sum(var1, var2) {
+  return var1 + var2;
+}
+function multiply(var1, var2) {
+  return var1 * var2;
+}
 function sumAndMultiply(var1, var2, var3) {
-  var total = document.getElementById('sumAndMultiplyTotal');
-  totalSum = (var1 + var2 + var3);
-  totalMultiply = (var1 * var2 * var3);
-  console.log('The total of ' + var1 + ' and ' + var2 + ' and ' + var3 + ' is ' + totalSum + '.');
-  sumAndMultiplyTotal.textContent = 'The total of ' + var1 + ' and ' + var2 + ' and ' + var3 + ' is ' + totalSum + '.';
-  console.log('The numbers ' + var1 + ' and ' + var2 + ' and ' + var3 + ' product is ' + totalMultiply + '.');
-  sumAndMultiplyTotal.textContent = 'The numbers ' + var1 + ' and ' + var2 + ' and ' + var3 + ' product is ' + totalMultiply + '.';
+  var array = [];
+  array.push(sum(sum(var1,var2),var3));
+  array.push(multiply(multiply(var1,var2),var3));
+  return array;
 }
