5a1c9cf71f6e818a07f59760f5ff954515da88ec
SEAN CHAMPAGNE <CHAMPAGNE.SEAN@GMAIL.COM>
2016-04-15 16:01:33 -0700

    completed the fourth question

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-function sumAndMultiply(var1, var2, var3) {
-  var total = document.getElementById('sumAndMultiplyTotal');
-  totalSum = (var1 + var2 + var3);
-  totalMultiply = (var1 * var2 * var3);
-  console.log('The total of ' + var1 + ' and ' + var2 + ' and ' + var3 + ' is ' + totalSum + '.');
-  sumAndMultiplyTotal.textContent = 'The total of ' + var1 + ' and ' + var2 + ' and ' + var3 + ' is ' + totalSum + '.';
-  console.log('The numbers ' + var1 + ' and ' + var2 + ' and ' + var3 + ' product is ' + totalMultiply + '.');
-  sumAndMultiplyTotal.textContent = 'The numbers ' + var1 + ' and ' + var2 + ' and ' + var3 + ' product is ' + totalMultiply + '.';
+
+function sumArray(array) {
+  // var array = [];
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  console.log("The numbers " + array + " have a product of " + sum);
+  return sum;
 }
